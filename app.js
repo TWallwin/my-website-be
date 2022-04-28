7a23a0a5e94b1f16091b357e9f629246093be44b
Tom wallwin <TWallwin>
2022-04-28 19:39:08 +0100

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { getViews, postView } = require("./controllers/views-controllers");
 const { invalidPathError } = require("./controllers/error-controllers");
 const { getEndpoints } = require("./controllers/endpoints-controllers");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 app.get("/api/views", getViews);
 app.get("/api", getEndpoints);
